c834b30cac449febcd1f520937c7412aad688267
Daniel Marchuk <dmarchuk@me.com>
2020-04-24 14:47:15 +0200

    Add index.ts to utils with re-exports of utilities used outside

---
+++ src/js/utils/index.ts
@@ -0,0 +1,6 @@
+export { createRectangleBlock } from './createRectangleBlock';
+export { generateRandomColor } from './generateRandomColor';
+export { getCoordinatesHash } from './getCoordinatesHash';
+export { getHeight } from './getHeight';
+export { getWidth } from './getWidth';
+export { rotateCoordinatesBy90Degrees } from './rotateCoordinatesBy90Degrees';
