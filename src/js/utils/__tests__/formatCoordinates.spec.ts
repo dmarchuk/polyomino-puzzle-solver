a12726c19e3f4fd74220a56d653e265d7a710770
Daniel Marchuk <dmarchuk@me.com>
2020-04-24 14:46:51 +0200

    Add formatCoordinates.ts utility with tests

---
+++ src/js/utils/__tests__/formatCoordinates.spec.ts
@@ -0,0 +1,27 @@
+import { formatCoordinates } from '../formatCoordinates';
+
+describe('formatCoordinates', () => {
+    it('generates formatted 2d array with L pattern', () => {
+        const coordinates = [{x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2}, {x: 0, y: 3}, {x: 1, y: 3}];
+        const result = formatCoordinates(coordinates);
+        const expected = [
+            [1, 0],
+            [1, 0],
+            [1, 0],
+            [1, 1],
+        ];
+
+        expect(result).toStrictEqual(expected);
+    });
+
+    it('generates formatted 2d array with U pattern', () => {
+        const coordinates = [{x: 0, y: 0}, {x: 2, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}];
+        const result = formatCoordinates(coordinates);
+        const expected = [
+            [1, 0, 1],
+            [1, 1, 1],
+        ];
+
+        expect(result).toStrictEqual(expected);
+    });
+});
---
+++ src/js/utils/formatCoordinates.ts
@@ -0,0 +1,16 @@
+import { Coordinate } from '../types';
+import { createRectangleBlock } from './createRectangleBlock';
+import { getWidth } from './getWidth';
+import { getHeight } from './getHeight';
+
+export const formatCoordinates = (coordinates: Coordinate[]) => {
+    const width = getWidth(coordinates);
+    const height = getHeight(coordinates);
+    const result = createRectangleBlock(width, height);
+
+    coordinates.forEach(({x, y}) => {
+        result[y][x] = 1;
+    });
+
+    return result;
+}
