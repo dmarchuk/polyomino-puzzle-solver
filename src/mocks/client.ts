74d12631b42f058a26799bf4efd683be0b925514
Daniel Marchuk <dmarchuk@me.com>
2020-04-26 15:05:22 +0200

    Add mock 'console.error' to mocks/client.ts

--- src/mocks/client.ts
+++ src/mocks/client.ts
@@ -3,3 +3,7 @@ import { JSDOM } from 'jsdom';
 const dom = new JSDOM();
 global.document = dom.window.document;
 global.window = dom.window;
+global.console = {
+    ...global.console,
+    error: jest.fn(),
+};
