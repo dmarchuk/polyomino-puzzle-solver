e60873b94a1e5c39f1ac957136c85197a4289351
Daniel Marchuk <dmarchuk@me.com>
2020-04-26 02:32:17 +0200

    Mock document and window for jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    setupFiles: [
+        './src/mocks/client.ts',
+    ],
 };
